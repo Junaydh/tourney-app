720f450e2379f8cac190de12dae9daa59e136dd8
Junayd H <junaydhus@gmail.com>
2023-02-20 15:14:36 -0800

    create match and matchlist components

---
+++ src/components/Match.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Match() {
+  return (
+    <article className='Match'>
+      <h1>
+        John <span>vs</span> Bob
+      </h1>
+      {}
+      <h2>Joe is the winner by 1!</h2>
+      {}
+      <h2>No winners yet!</h2>
+    </article>
+  );
+}
+
+export default Match;
