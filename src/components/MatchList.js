4343860d9d22ea7ef7656e26398ba00a33e1cfc8
Junayd H <junaydhus@gmail.com>
2023-02-20 16:50:55 -0800

    refactored playerlist component to render all matches in matchData

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Match from './Match';
 
 function MatchList(props) {
-  const oneMatch = props.matchData[0];
+  const matches = props.matchData.map((match) => {
+    return <Match {...match} key={match.matchNumber}/>
+  });
 
   return (
     <section className='PlayerList MatchList'>
       <h1>Match list</h1>
-      <Match {...oneMatch} />
+      {matches}
     </section>
   );
 }
